d15804aa09075063608fbc81380936fc38f3ffca
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2017-04-06 23:48:30 +0000

    learned about lifetimes

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
+// Include crates
 #[macro_use]
 extern crate log;
+extern crate clap;
 
+// Pull in watcher module
 pub mod watcher;
+
+
+fn build_cli<'a, 'b>() -> clap::App<'a, 'b> {
+    let app = clap::App::new("fnr");
+
+    return app;
+}
 
 // search all files recursively by extension
 //
 fn main() {
     println!("Hello, world!");
+
+    let cli = build_cli();
 }
