bab002dab729f2b3e3310f15fcc8be526affcf98
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2017-04-07 00:40:49 +0000

    build cli app

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,22 @@ extern crate clap;
 pub mod watcher;
 
 
+/// build_cli build's the cli app.
 fn build_cli<'a, 'b>() -> clap::App<'a, 'b> {
-    let app = clap::App::new("fnr");
+    let app = clap::App::new("fnr")
+        .version("0.1")
+        .author("Manuel A. Rodriguez <manuel@rdrsss.io>")
+        .about("Finds and replaces text within text files.")
+        .arg(clap::Arg::with_name("find")
+                 .short("f")
+                 .long("find")
+                 .help("The string to find")
+                 .takes_value(true))
+        .arg(clap::Arg::with_name("replace")
+                 .short("r")
+                 .long("replace")
+                 .help("The string to replace with")
+                 .takes_value(true));
 
     return app;
 }
@@ -16,7 +30,9 @@ fn build_cli<'a, 'b>() -> clap::App<'a, 'b> {
 // search all files recursively by extension
 //
 fn main() {
-    println!("Hello, world!");
-
+    // Build the cli app
     let cli = build_cli();
+
+    // Extract matches
+    let matches = cli.get_matches();
 }
