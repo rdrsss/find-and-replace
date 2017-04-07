70758c450bce8823b4368a62e56f04a849b511c0
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2017-04-07 20:23:33 +0000

    stubbed out search file logic

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@
 extern crate log;
 extern crate clap;
 
-use std::fs;
 use std::path;
 use std::env;
+use std::string;
 
 // Pull in watcher module
 pub mod watcher;
@@ -31,13 +31,17 @@ fn build_cli<'a, 'b>() -> clap::App<'a, 'b> {
                  .short("p")
                  .long("path")
                  .help("Path of directory for search to take place, defaults to current dir")
+                 .takes_value(true))
+        .arg(clap::Arg::with_name("extension")
+                 .short("e")
+                 .long("extension")
+                 .help("Filter out files by extension")
                  .takes_value(true));
 
     return app;
 }
 
 // search all files recursively by extension
-//
 fn main() {
     // Build the cli app
     let cli = build_cli();
@@ -56,16 +60,30 @@ fn main() {
             // Make sure is directory
             if passed_in.is_dir() {
                 // Set the path to search in.
-                path = passed_in
+                path = passed_in.to_path_buf();
             }
         }
     }
 
+    let mut ext = string::String::from("txt");
+
     // Construct the file list.
+    let pres = watcher::construct_file_list(&path, &ext);
 
     if let Some(f) = matches.value_of("find") {
         println!("finding");
         // Run find on file list.
+        //
+        if pres.is_ok() {
+            let paths = pres.unwrap();
+            for fp in paths {
+                println!("{:?}", fp);
+                let _ = watcher::search_file(&fp);
+            }
+
+        } else {
+            // TODO: Report error or some shit.
+        }
         // Construct found instances db.
     }
 
