6c13e9af95929a6868e3ac36b06df5b34f4651e1
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2017-04-18 17:14:05 +0000

    add get tests dir helper function

--- tests/file_test.rs
+++ tests/file_test.rs
@@ -5,8 +5,27 @@
 #[cfg(test)]
 mod tests {
 
+    use std::fs;
+    use std::env;
+    use std::path;
+
+    /// get_tests_path will construct a path to the tests directory from the top level
+    /// project directory.
+    fn get_tests_path() -> path::PathBuf {
+        let mut path = env::current_dir().unwrap();
+        path.push("tests");
+        return path;
+    }
+
+    /// setup_test_file copies loremipsum.txt to be modified by the tests.
+    fn setup_test_file() {
+        let path = get_tests_path();
+        println!("{:?}", path);
+    }
+
     #[test]
     fn lorem_ipsum_test() {
+        setup_test_file();
         // copy text file to be modified.
 
         // find and replace instances of lorem
