97d44e6ea1caa363e73e98b7544d45adfa4fe117
Manuel A. Rodriguez <manuel.rdrs@gmail.com>
2017-04-18 17:35:22 +0000

    setup lorem ipsum test

--- tests/file_test.rs
+++ tests/file_test.rs
@@ -9,6 +9,9 @@ mod tests {
     use std::env;
     use std::path;
 
+    const TEST_FILE_NAME: &'static str = "loremipsum.txt";
+    const TEST_COPY_NAME: &'static str = "copy_ipsum.txt";
+
     /// get_tests_path will construct a path to the tests directory from the top level
     /// project directory.
     fn get_tests_path() -> path::PathBuf {
@@ -18,17 +21,60 @@ mod tests {
     }
 
     /// setup_test_file copies loremipsum.txt to be modified by the tests.
-    fn setup_test_file() {
+    fn setup_test_file() -> bool {
         let tests_path = get_tests_path();
-        let mut file_path = tests_path + "loremipsum.txt";
+
+        let mut file_path = tests_path.clone();
+        file_path.push(TEST_FILE_NAME);
+
+        let mut copy_path = tests_path.clone();
+        copy_path.push(TEST_COPY_NAME);
+
+        println!("file path : {:?}", file_path);
+        println!("copy_path : {:?}", copy_path);
+
+        let res = fs::copy(file_path, copy_path);
+        match res {
+            Ok(_) => {}
+            Err(evc) => {
+                println!("{:?}", evc);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /// delete_test_file delete's the test file that was created if it exists.
+    fn delete_test_file() -> bool {
+        let tests_path = get_tests_path();
+        let mut copy_path = tests_path.clone();
+        copy_path.push(TEST_COPY_NAME);
+
+        if !copy_path.exists() {
+            return false;
+        }
+
+        let res = fs::remove_file(copy_path);
+        match res {
+            Ok(_) => {}
+            Err(rerr) => {
+                println!("{:?}", rerr);
+                return false;
+            }
+        }
+
+        return true;
     }
 
     #[test]
     fn lorem_ipsum_test() {
+        // Setup test file for finding and replacing.
         setup_test_file();
-        // copy text file to be modified.
 
-        // find and replace instances of lorem
+        // Find and replace instances of lorem.
+
+        // Delete test file.
+        delete_test_file();
     }
 
 }
